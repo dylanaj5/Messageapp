1645505bf5cbe81f1818b9ee6349e8771da9ddbb
dylanaj5 <dylanaj5@gmail.com>
2015-07-26 19:10:18 +0100

    Adding Files

---
+++ app/controllers/user/sessions_controller.rb
@@ -0,0 +1,37 @@
+class User::SessionsController < Devise::SessionsController
+  helper_method :verify_recaptcha
+  skip_before_filter :require_no_authentication, :only => [:new]
+  
+  def create
+    if verify_recaptcha
+      super
+    else
+      build_resource
+      respond_with_navigational(resource) { render :new }
+    end    
+  end
+
+# before_filter :configure_sign_in_params, only: [:create]
+
+    #GET /resource/sign_in
+    #super
+    #verify_recaptcha(:model => @User, :message => "Oh! It's an error with reCAPTCHA!")
+    #end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
+  # end
+end
